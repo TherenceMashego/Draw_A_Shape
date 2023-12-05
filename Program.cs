a58c858408be59719c8eefad7ff2027f0ca70e20
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 09:05:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_A_Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This will draw a triangle
+
+            Console.WriteLine("        /| ");
+            Console.WriteLine("       / | ");
+            Console.WriteLine("      /  | ");
+            Console.WriteLine("     /   | ");
+            Console.WriteLine("    /    | ");
+            Console.WriteLine("   -------");
+
+            // freeze the frame
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
